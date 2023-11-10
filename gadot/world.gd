f348ee98ad4a92332e21d087b1c695a9b27248ad
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-11-09 21:03:43 -0500

    added fullscreen, and improved crosshair

--- gadot/world.gd
+++ gadot/world.gd
@@ -13,6 +13,14 @@ var enet_peer = ENetMultiplayerPeer.new()
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("fullscreen"):
+		swap_fullscreen_mode()
+		
+func swap_fullscreen_mode():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_MAXIMIZED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 
 func _on_offline_button_pressed():
 	main_menu.hide()
